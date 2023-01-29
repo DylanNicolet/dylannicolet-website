b3973b7893bbeaf8972e991e71674278737cff0e
DylanLaptop <dylannicolet@hotmail.com>
2023-01-29 15:16:34 +0400

    Implemented: Flying stars animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from './Pages/Hero.jsx';
+import Profile from './Pages/Profile.jsx';
 import "./sass/App.css";
 
 function App() {
   return (
     <div className="App">
       <Hero/>
+      <Profile/>
     </div>
   );
 }
