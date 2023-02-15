a1871f1120163ff7686e06cbb7e47b9f22675b34
DylanLaptop <dylannicolet@hotmail.com>
2023-02-15 18:10:51 +0400

    Added: Loading Spinner & missing logos to public folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import LoadingSpinner from './components/LoadingSpinner.jsx';
 import Hero from './Pages/Hero.jsx';
 import Profile from './Pages/Profile.jsx';
 import FeaturedProjects from './Pages/FeaturedProjects.jsx';
 import AboutMe from './Pages/AboutMe.jsx';
-import "./sass/App.css";
 import Contact from './Pages/Contact.jsx';
+import "./sass/App.css";
 
 function App() {
   return (
     <div className="App">
+      <LoadingSpinner />
       <Hero/>
       <Profile/>
       <FeaturedProjects />
---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function LoadingSpinner(){
+    return(
+        <section className="loading-spinner">
+            <p className="loading-spinner__text">CODING</p>
+            <p className="dot-1">.</p>
+            <p className="dot-2">.</p>
+            <p className="dot-3">.</p>
+        </section>
+    )
+}
