7f640565163a031b7530304f7bf97ee6e454936c
DylanLaptop <dylannicolet@hotmail.com>
2023-10-15 22:01:56 +0400

    [INTE] Add preview iframes to Featured Projects

--- src/components/FeaturedCard.jsx
+++ src/components/FeaturedCard.jsx
@@ -1,17 +1,47 @@
 import React from "react";
 
 export default function FeaturedCard(props){
+    // States
+    let [previewActive, setPreviewActive] = React.useState(false)
+
     let techUsed = props.techUsed.map((tech, index) => {
         return(
             <img src={tech} alt="tech logo"  key={index} className="featured-card__tech" />
         )
     })
 
+    function activatePreview() {
+        setPreviewActive(true)
+    }
+
+    function deactivatePreview() {
+        setPreviewActive(false)
+    }
+
+    // Close the preview when the mouse leaves the popup
+    function handleMouseLeave() {
+        if (previewActive) {
+          deactivatePreview();
+        }
+    }
+
     return(
         <section className="featured-card">
             <section className="featured-card__modal">
+                { (props.title !== "Component library") && // Don't generate live preview for component library
+                    <button className="button-primary" onClick={activatePreview}>Mini preview</button>
+                }
+                <a href={props.live}  target="_blank" rel="noreferrer"><button className="button-primary">Live website</button></a>
                 <a href={props.github} target="_blank" rel="noreferrer"><button className="button-primary">GitHub Code</button> </a>
-                <a href={props.live}  target="_blank" rel="noreferrer"><button className="button-primary">Live preview</button></a> 
+
+                { previewActive &&
+                    <div className="featured-card__preview" onMouseLeave={handleMouseLeave}>
+                        <div className="featured-card__preview-container">
+                            <button onClick={deactivatePreview} className="button__close-preview">+</button>
+                            <iframe src={props.live} title="Live preview of the website"></iframe>
+                        </div>
+                    </div>
+                }
             </section>
 
             <img src={props.imageSource} alt="thumbnail preview of the website"  className="featured-card__image"/>
