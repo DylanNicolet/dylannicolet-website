c79648a70d613cffd9f3d24a902c60fc0a8c989c
Dylan Nicolet <dylannicolet@hotmail.com>
2025-04-30 14:35:31 +0400

    [INTE] Added new icons to Hero tech bar

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import git from "../Images/git-logo.png"
+import html from "../Images/html-logo.png"
 import css from "../Images/css-logo.png"
 import sass from "../Images/sass-logo.png"
+import js from "../Images/js-logo.png"
 import react from "../Images/react-logo.png"
 import redux from "../Images/redux-logo.png"
-import js from "../Images/js-logo.png"
-import github from "../Images/github-logo.png"
+import zustand from "../Images/zustand-logo.png"
+import router from "../Images/router-logo.png"
 
 export default function Hero(){
 
@@ -34,12 +37,15 @@ export default function Hero(){
             </p>
             
             <section className="hero__tech-icon-container">
-                <img src={css} alt="Css logo" className="hero__tech-icon"/>
-                <img src={sass} alt="Sass logo"  className="hero__tech-icon"/>
-                <img src={react} alt="React logo"  className="hero__tech-icon"/>
-                <img src={redux} alt="Redux"  className="hero__tech-icon"/>
-                <img src={js} alt="JavaScript logo"  className="hero__tech-icon"/>
-                <img src={github} alt="GitHub logo"  className="hero__tech-icon"/>
+                <img src={git} alt="GitHub"  className="hero__tech-icon"/>
+                <img src={html} alt="HTML 5" className="hero__tech-icon"/>
+                <img src={css} alt="CSS 3" className="hero__tech-icon"/>
+                <img src={sass} alt="SASS/SCSS"  className="hero__tech-icon"/>
+                <img src={js} alt="JavaScript"  className="hero__tech-icon"/>
+                <img src={react} alt="React"  className="hero__tech-icon"/>
+                <img src={redux} alt="React Redux"  className="hero__tech-icon"/>
+                <img src={zustand} alt="React Redux"  className="hero__tech-icon"/>
+                <img src={router} alt="React Router"  className="hero__tech-icon"/>
             </section>
 
             <section className="hero__stars-container">
